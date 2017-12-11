d285b1166e2ddf76eb3e18a18915ee0fb3e80421
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2017-12-11 12:04:10 +0100

    Updated SES example since results can now be replicated exactly

--- exampleSES.R
+++ exampleSES.R
@@ -14,12 +14,12 @@ TDARC601 <- createTD(data = dfARC601, genotype = "Seedname",
                      colCoordinates = "X", design = "rowcol")
 
 ## Perform SpATS analysis for computing BLUEs. - step 5 in SES workflow
-ARC601Sp <- STRunModel(TDARC601, traits = traitnames, engine = "SpATS")
+ARC601Sp <- STRunModel(TDARC601, traits = traitnames, engine = "SpATS",
+                       control = list(nSeg = c(ceiling(nlevels(TDARC601$colId) / 2),
+                                               nlevels(TDARC601$rowId))))
 BLUEsSp <- STExtract(ARC601Sp, what = "BLUEs")
 
-## Comparison for some traits.
-## Small differences because of difference in number of segments
-## for rows in SpATS analysis.
+## Comparison for THA and pctS.
 BLUEsOrig <- BLUEs[BLUEs$FieldId == "ARC601", ]
 plot(x = BLUEsSp$THA, y = BLUEsOrig$THA)
 abline(a = 0, b = 1, col = "blue")
@@ -27,30 +27,36 @@ plot(x = BLUEsSp$pctS, y = BLUEsOrig$pctS)
 abline(a = 0, b = 1, col = "blue")
 
 ## Perform analysis using asreml and lme4. - step 7 in SES workflow
-## lme4 used for extra comparison.
 ## traits restricted to "THA" for speed
-ARC601As <- STRunModel(TDARC601, traits = "THA", engine = "asreml")
+## For asreml model only fitted with genotype fixed replicate results
+ARC601As <- STRunModel(TDARC601, traits = "THA", engine = "asreml", what = "fixed")
 BLUEsAs <- STExtract(ARC601As, what = "BLUEs")
+## Model also fitted using for extra comparison.
 ARC601Lm <- STRunModel(TDARC601, traits = "THA", engine = "lme4")
 BLUEsLm <- STExtract(ARC601Lm, what = "BLUEs")
 
-## Comparison for some THA.
-## Plots seem perfect, but there are small differences when using asreml.
-## This is because in SES the model is fitted in one step whereas in RAP
-## it is done in two steps and some variance components are fixed in between.
+## Comparison for THA.
 RCCorrectedOrig <- RCcorrected[RCcorrected$FieldId == "ARC601", ]
-meanDifAs <- mean(abs(RCCorrectedOrig$THA - BLUEsAs$THA))
-meanDifLm <- mean(abs(RCCorrectedOrig$THA - BLUEsLm$THA))
 plot(x = BLUEsAs$THA, y = RCCorrectedOrig$THA)
 abline(a = 0, b = 1, col = "blue")
 plot(x = BLUEsLm$THA, y = RCCorrectedOrig$THA)
 abline(a = 0, b = 1, col = "blue")
 
 ## Comparison for heritabilities. - step 10 in SES workflow
-## Again small differences because of difference in number of segments
-## for rows in SpATS analysis.
-herit <- STExtract(ARC601Sp, what = "heritability")
+## Create new TD object since for this calculation column Geno is used as genotype
+## and "Check" as a fixed factor
+TDARC601H <- createTD(data = dfARC601, genotype = "Geno", checkId = "Check",
+                     rowId = "Yf", colId = "Xf", rowCoordinates = "Y",
+                     colCoordinates = "X", design = "rowcol")
+## Only fit model with genotype random.
+## Because of the structure of the data, genotype fixed leads to collinearity
+ARC601SpH <- STRunModel(TDARC601H, traits = traitnames, what = "random",
+                        useCheckId = TRUE, engine = "SpATS",
+                        control = list(nSeg = c(ceiling(nlevels(TDARC601$colId) / 2),
+                                                nlevels(TDARC601$rowId))))
+herit <- STExtract(ARC601SpH, what = "heritability")
 heritOrig <- heritabilities["ARC601", ]
 plot(x = herit, y = heritOrig)
 abline(a = 0, b = 1, col = "blue")
+
 
