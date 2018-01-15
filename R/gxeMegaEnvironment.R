e838b6746597486056249bb7afdd91ee58e5ebcf
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2018-01-15 16:43:07 +0100

    Simplified gxeMegaEnvironment

--- R/gxeMegaEnvironment.R
+++ R/gxeMegaEnvironment.R
@@ -1,19 +1,19 @@
-#' Form mega-environments based on winning genotypes from an AMMI-2 model
+#' Form mega-environments based on winning genotypes from an AMMI model
 #'
-#' This function fits an AMMI-2 model and then using the fitted values produces
-#' a new factor based on the winning genotype in each environment. This new factor is
-#' added as a column megaEnv to the input data. If a column megaEnv already exists the
-#' existing data is overwritten with a warning.
+#' This function fits an AMMI model and then using the fitted values produces
+#' a new factor based on the winning genotype in each environment. This factor is
+#' added as a column megaEnv to the input data. If a column megaEnv already
+#' exists the existing data is overwritten with a warning.
 #'
 #' @inheritParams gxeAmmi
 #'
-#' @param method A criterion to determine the best trait, either \code{"max"} or \code{"min"}.
-#' @param summaryTable A logical specifying whether a summary table will be returned.
+#' @param method A character sting indicating the criterion to determine
+#' the best trait, either \code{"max"} or \code{"min"}.
+#' @param summaryTable Should a summary table will be printed?
 #'
 #' @return The input object of class \code{\link{TD}} with an added extra column megaEnv.
 #'
 #' @examples
-#' data(TDMaize)
 #' TDmegaEnv <- gxeMegaEnvironment(TD = TDMaize, trait = "yld")
 #' attr(TDmegaEnv, "summary")
 #'
@@ -21,7 +21,7 @@
 
 gxeMegaEnvironment <- function(TD,
                                trait,
-                               method = "max",
+                               method = c("max", "min"),
                                summaryTable = TRUE) {
   if (missing(TD) || !inherits(TD, "TD")) {
     stop("TD should be a valid object of class TD.\n")
@@ -36,46 +36,34 @@ gxeMegaEnvironment <- function(TD,
       !trait %in% colnames(TD)) {
     stop("trait has to be a column in TD.\n")
   }
-  if (is.null(method) || !is.character(method) || length(method) > 1 ||
-      !method %in% c("min", "max")) {
-    stop("method should be either min or max.\n")
-  }
+  method <- match.arg(method)
   ## drop factor levels
   TD$genotype <- droplevels(TD$genotype)
   TD$env <- droplevels(TD$env)
-  ## use AMMI
+  ## Perform AMMI anlaysis.
   AMMI <- gxeAmmi(TD = TD, trait = trait, nPC = 2)
   fitted <- AMMI$fitted
-  genoNames <- rownames(fitted)
-  envNames  <- colnames(fitted)
-  if (method == "max") {
-    winPosition <- apply(X = fitted, MARGIN = 2, FUN = which.max)
-  } else {
-    winPosition <- apply(X = fitted, MARGIN = 2, FUN = which.min)
-  }
-  winGeno <- genoNames[winPosition]
-  megaFactor <- factor(winGeno, labels = 1:length(unique(winGeno)))
-  ## re-labelling
-  levels(megaFactor) <- unique(as.integer(megaFactor))
-  ## merge factor levels
-  megaEnvFactor <- TD$env
-  levels(megaEnvFactor) <- as.character(megaFactor)
-  TD$megaEnv <- megaEnvFactor
-  ## re-labelling
-  levels(TD$megaEnv) <- 1:nlevels(megaEnvFactor)
+  ## Extract position of best genotype per environment.
+  winPosition <- apply(X = fitted, MARGIN = 2,
+                       FUN = getFunction(paste0("which.", method)))
+  ## Extract best genotype per environment.
+  winGeno <- rownames(fitted)[winPosition]
+  ## Create factor based on best genotypes.
+  megaFactor <- factor(winGeno, labels = "")
+  ## Merge factor levels to original data.
+  TD$megaEnv <- TD$env
+  levels(TD$megaEnv) <- as.character(megaFactor)
   if (summaryTable) {
-    summTab <- matrix(nrow = length(envNames), ncol = 4)
-    colnames(summTab) <- c("Mega env", "env", "genotype", "AMMI estimates")
-    megaOrder <- order(megaFactor)
-    summTab[, 1] <- megaFactor
-    summTab[, 2] <- envNames
-    summTab[, 3] <- winGeno
-    summTab[, 4] <- sapply(X = 1:length(envNames), FUN = function(x) {
-      signif(fitted[winPosition[x], x], 5)
-    })
-    summTab <- as.data.frame(summTab[megaOrder, ], stringsAsFactors = FALSE)
-    summTab[, 4] <- as.numeric(summTab[, 4])
+    ## Create summary table.
+    summTab <- data.frame(megaFactor, envNames = colnames(fitted), winGeno,
+                          "AMMI estimates" = sapply(X = 1:ncol(fitted),
+                                                    FUN = function(x) {
+                                                      signif(fitted[winPosition[x], x], 5)
+                                                    }),
+                          check.names = FALSE)
+    summTab <- summTab[order(megaFactor), ]
     attr(TD, "summary") <- summTab
+    print(summTab)
   }
   return(TD)
 }
