2cc5443e251aa0a067b5a90b326ec9de5624c4bd
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2018-03-26 12:24:15 +0200

    Moved outlierTD to redundant and modified outlierSSA to use new structure

--- R/outliers.R
+++ R/outliers.R
@@ -1,87 +1,3 @@
-#' Identifying outliers in objects of class TD
-#'
-#' Function to identify observations that exceed \code{coef} times the
-#' interquartile range. A summary is printed of outliers and observations
-#' that have the same value for \code{commonFactors}. The latter ones will
-#' be marked as similar to distinguish them from the former ones.
-#'
-#' @param TD An object of class \code{\link{TD}}.
-#' @param traits A character vector specifying the names of the traits for
-#' which outliers should be identified.
-#' @param coef A numerical value used for determining when a value is
-#' considered an outlier. All observations that exceed \code{coef} times
-#' the interquartile range will be marked as outliers.
-#' @param commonFactors A character vector specifying the names of columns
-#' in \code{TD} used for selecting observations that are similar to the
-#' outliers. If \code{commonFactors = NULL} only outliers are reported and
-#' no similar observations.
-#'
-#' @return A data.frame containing logical values indicating if the
-#' observation is an outlier.
-#'
-#' @examples
-#' ## Detect outliers in TDHeat05.
-#' outliers <- outlierTD(TD = TDHeat05, traits = "yield")
-#'
-#' @export
-outlierTD <- function(TD,
-                      traits,
-                      coef = 1.5,
-                      commonFactors = NULL) {
-  ## Checks.
-  if (missing(TD) || !inherits(TD, "TD")) {
-    stop("TD should be a valid object of class TD.\n")
-  }
-  if (is.null(traits) || !is.character(traits) || !all(traits %in% colnames(TD))) {
-    stop("traits has to be a vector of columns in TD.\n")
-  }
-  if (!is.null(commonFactors) && !is.character(commonFactors) &&
-      !all(commonFactors %in% colnames(TD))) {
-    stop("commonFactor has to be a vector of columns in TD.\n")
-  }
-  if (is.null(coef) || !is.numeric(coef) || length(coef) > 1 || coef < 0) {
-    stop("coef should be a positive numerical value.\n")
-  }
-  ## Create empty data.frame for storing results.
-  indicator <- data.frame(matrix(data = FALSE, nrow = nrow(TD),
-                                 ncol = length(traits),
-                                 dimnames = list(NULL, traits)))
-  outTrait <- vector(mode = "list", length = length(traits)) %>%
-    setNames(traits)
-  for (trait in traits) {
-    ## Set commonFactors to trait when empty so joining is always possible.
-    if (is.null(commonFactors)) {
-      commonFactors <- trait
-    }
-    ## Compute outliers.
-    outVals <- boxplot.stats(x = TD[[trait]], coef = coef)$out
-    if (length(outVals > 0)) {
-      ## Fill indicator column for current trait.
-      indicator[[trait]] <- TD[[trait]] %in% outVals
-      ## Create data.frame with outliers for current trait.
-      outTrait[[trait]] <- dplyr::semi_join(TD, TD[TD[[trait]] %in% outVals,
-                                                   commonFactors, drop = FALSE],
-                                            by = commonFactors) %>%
-        ## Add column for similar and column trait with the current trait.
-        dplyr::mutate(similar = !.[[trait]] %in% outVals, trait = trait) %>%
-        ## Rename column trait to "value"
-        dplyr::rename("value" = !!trait) %>%
-        ## Change order of columns so always display trait and value first.
-        dplyr::select(!!rlang::sym("trait"), !!rlang::sym("value"),
-                      dplyr::everything())
-    }
-  }
-  ## Create one single outlier matrix.
-  pMat <- Reduce(f = rbind, x = outTrait)
-  if (!is.null(pMat)) {
-    cat(paste("Observations that exceed", coef, "times the interquartile range\n\n"))
-    print(format(pMat, quote = FALSE))
-  } else {
-    cat(paste("No observations that exceed", coef, "times the interquartile range.\n"))
-  }
-  invisible(indicator)
-}
-
 #' Identifying outliers in objects of class SSA
 #'
 #' Function to identify observations with standardized residuals exceeding
@@ -93,12 +9,19 @@ outlierTD <- function(TD,
 #' value for \code{commonFactors}. The latter ones will be marked as
 #' similar to distinguish them from the former ones.
 #'
-#' @inheritParams outlierTD
-#'
 #' @param SSA An object of class \code{\link{SSA}}.
+#' @param trial A character string specifying the trial for which outliers
+#' should be identified. If \code{NULL} and \code{SSA} contains only one trial
+#' that trial is used.
+#' @param traits A character vector specifying the names of the traits for
+#' which outliers should be identified.
 #' @param rLimit A numerical value used for determining when a value is
 #' considered an outlier. All observations with standardized residuals
 #' exceeding \code{rLimit} will be marked as outliers.
+#' @param commonFactors A character vector specifying the names of columns
+#' in \code{TD} used for selecting observations that are similar to the
+#' outliers. If \code{commonFactors = NULL} only outliers are reported and
+#' no similar observations.
 #'
 #' @return A data.frame containing logical values indicating if the
 #' observation is an outlier.
@@ -112,6 +35,7 @@ outlierTD <- function(TD,
 #'
 #' @export
 outlierSSA <- function(SSA,
+                       trial = NULL,
                        traits,
                        rLimit = NULL,
                        commonFactors = NULL) {
@@ -119,21 +43,35 @@ outlierSSA <- function(SSA,
   if (missing(SSA) || !inherits(SSA, "SSA")) {
     stop("SSA should be a valid object of class SSA.\n")
   }
-  if (is.null(traits) || !is.character(traits) || !all(traits %in% colnames(SSA$TD))) {
-    stop("traits has to be a vector of columns in TD.\n")
+  if (is.null(trial) && length(SSA) > 1) {
+    stop("No trial provided but multiple trials found in SSA object.\n")
+  }
+  if (!is.null(trial) && (!is.character(trial) || length(trial) > 1 ||
+                          !trial %in% names(SSA))) {
+    stop("Trial has to be a single character string defining a trial in SSA.\n")
+  }
+  if (is.null(trial)) {
+    trial <- names(SSA)
+  }
+  if (!is.null(traits) && (!is.character(traits) ||
+                           !all(traits %in% colnames(SSA[[trial]]$TD[[trial]])))) {
+    stop("Trait has to be a character vector defining columns in TD.\n")
   }
   if (!is.null(commonFactors) && !is.character(commonFactors) &&
-      !all(commonFactors %in% colnames(SSA$TD))) {
-    stop("commonFactor has to be a vector of columns in TD.\n")
+      !all(commonFactors %in% colnames(SSA[[trial]]$TD[[trial]]))) {
+    stop("commonFactor has to be a character vector defining columns in TD.\n")
   }
   if (!is.null(rLimit) && (!is.numeric(rLimit) || length(rLimit) > 1 ||
                            rLimit < 0)) {
     stop("rLimit should be NULL or a positive numerical value.\n")
   }
-  stdRes <- STExtract(SSA, traits = traits, what = "stdRes")
-  rDf <- STExtract(SSA, traits = traits, what = "rDf")
+  stdRes <- STExtract(SSA, trials = trial, traits = traits,
+                      what = "stdRes")[[trial]]
+  rDf <- STExtract(SSA, trials = trial, traits = traits,
+                   what = "rDf")[[trial]]
   ## Create empty data.frame for storing results.
-  indicator <- data.frame(matrix(data = FALSE, nrow = nrow(SSA$TD),
+  indicator <- data.frame(matrix(data = FALSE,
+                                 nrow = nrow(SSA[[trial]]$TD[[trial]]),
                                  ncol = length(traits),
                                  dimnames = list(NULL, traits)))
   outTrait <- vector(mode = "list", length = length(traits)) %>%
@@ -155,8 +93,10 @@ outlierSSA <- function(SSA,
       ## Rename column for easier joining.
       stdRes <- dplyr::rename(stdRes, "res" = !!trait)
       ## Create data.frame with outliers for current trait.
-      outTrait[[trait]] <- dplyr::inner_join(SSA$TD, stdRes,
-                                             by = setdiff(colnames(stdRes), "res")) %>%
+      outTrait[[trait]] <- dplyr::inner_join(SSA[[trial]]$TD[[trial]],
+                                             stdRes,
+                                             by = setdiff(colnames(stdRes),
+                                                          "res")) %>%
         dplyr::semi_join(.[.[["res"]] %in% outVals, commonFactors,
                            drop = FALSE], by = commonFactors) %>%
         ## Add column for similar and column trait with the current trait.
