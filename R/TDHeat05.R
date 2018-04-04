5072dafa1e6b8b5f4414e13d68a168d206b37e22
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2018-04-04 12:03:29 +0200

    Added documentation for test cross objects

--- R/TDHeat05.R
+++ R/TDHeat05.R
@@ -6,3 +6,12 @@
 
 #' Random test data for unit testing.
 "testData"
+
+#' Fake data of f2 cross for unit testing.
+"testF2"
+
+#' Fake data of 4way cross for unit testing.
+"test4way"
+
+#' Fake data of backcross for unit testing.
+"testBc"
