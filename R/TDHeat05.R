c29660e2df7bb31cd61ebfa9b694670a26e8b402
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2018-04-10 11:56:49 +0200

    Moved test datasets to internal

--- R/TDHeat05.R
+++ R/TDHeat05.R
@@ -5,13 +5,17 @@
 "TDMaize"
 
 #' Random test data for unit testing.
+#' @keywords internal
 "testData"
 
 #' Fake data of f2 cross for unit testing.
+#' @keywords internal
 "testF2"
 
 #' Fake data of 4way cross for unit testing.
+#' @keywords internal
 "test4way"
 
 #' Fake data of backcross for unit testing.
+#' @keywords internal
 "testBc"
