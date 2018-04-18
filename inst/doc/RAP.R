27b09885458566c0852d87df09e4a4b7b7c0fe83
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2018-04-18 15:08:36 +0200

    Replaced wheatAus by wheatChl in vignette

--- inst/doc/RAP.R
+++ inst/doc/RAP.R
@@ -1,6 +1,44 @@
 ## ----setup, include = FALSE----------------------------------------------
 knitr::opts_chunk$set(
-  collapse = TRUE,
-  comment = "#>"
+collapse = TRUE,
+comment = "#>"
 )
+library(RAP)
+
+## ----createTD------------------------------------------------------------
+data("wheatChl")
+wheatTD <- createTD(data = wheatChl[wheatChl$trial != "C_SWS_12", ], 
+                    genotype = "trt_id", repId = "rep", subBlock = "bl", 
+                    rowId = "row", colId = "col", rowCoord = "row", 
+                    colCoord = "col")
+
+## ----getMeta-------------------------------------------------------------
+wheatMeta <- getMeta(TD = wheatTD)
+head(wheatMeta)
+
+## ----setMeta-------------------------------------------------------------
+wheatMeta$trLocation <- "Santa Rosa"
+wheatMeta$trDate <- as.Date(rep(c("310811", "310812"), times = 2), "%d%m%y")
+wheatMeta$trLat <- -36.32
+wheatMeta$trLong <- -71.55
+wheatMeta$trPlotLength = 2
+wheatMeta$trPlotWidth = 1
+wheatTD <- setMeta(TD = wheatTD, meta = wheatMeta)
+
+## ----TDsum---------------------------------------------------------------
+summary(wheatTD, trial = "SR_FI_11", traits = "GY")
+
+## ----layoutPlot----------------------------------------------------------
+plot(wheatTD, trials = "SR_FI_11")
+
+## ----mapPlot-------------------------------------------------------------
+plot(wheatTD, plotType = "map")
+
+## ----fitSp, message=FALSE------------------------------------------------
+modWheat <- STRunModel(TD = wheatTD, trials = "SR_FI_11", traits = "GY",
+                       design = "res.rowcol")
+
+## ----fitSum, message=FALSE-----------------------------------------------
+## Set nBest to 5 to decrease size of output.
+summary(modWheat, nBest = 5)
 
