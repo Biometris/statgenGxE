0ceafb57d00e572a7fd6c76c3d658f7b53a4d5a1
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2018-04-19 12:20:31 +0200

    Modified exampleBrapi to function again after update of RAP and BRAPI

--- exampleBrapi.R
+++ exampleBrapi.R
@@ -1,12 +1,14 @@
+library(RAP)
 ## Get latest version
 devtools::install_github("CIP-RIU/brapi")
 
 ## BrAPI Connection object for Authentication on BMS
 bmscon <- brapi::ba_connect(brapiDb = NULL, secure = FALSE, protocol = "http://",
-                            db = "34.226.132.187", port = 48080, apipath = "bmsapi",
-                            multicrop = TRUE, crop = "", user = "wur",
-                            password = "27Aqua74", token = "", granttype = "password",
-                            clientid = "rbrapi", bms = TRUE)
+                            db = "34.226.132.187", port = 48080,
+                            apipath = "bmsapi", multicrop = TRUE, crop = "",
+                            user = "wur", password = "27Aqua74", token = "",
+                            granttype = "password", clientid = "rbrapi",
+                            bms = TRUE)
 ## Authenticate user with password and obtain the session token
 bmscon <- brapi::ba_login(con = bmscon)
 ## Select "wheat" as crop and write to connection object
@@ -17,31 +19,31 @@ studyTablesList <- lapply(studyDbIds, function(id) {
   brapi::ba_studies_table(con = bmscon, studyDbId = id)
 })
 ## Create one data.frame with data for all fields.
-studyTableTot <- Reduce("rbind2", studyTablesList)
+studyTableTot <- Reduce("rbind", studyTablesList)
 ## Remove data for sites 3, 13, 15 and 17 (failed crop)
 studyTableTot <- studyTableTot[!studyTableTot$locationName %in%
-                                            c("Site03", "Site13", "Site15", "Site17"), ]
+                                 c("Site03", "Site13", "Site15", "Site17"), ]
 studyTableTot$locationName <- droplevels(studyTableTot$locationName)
 ## Rename trait column for ease of use
-colnames(studyTableTot)[which(colnames(studyTableTot) == "GY_Calc_tha|22661")] <- "GY_Calc_tha"
+colnames(studyTableTot)[colnames(studyTableTot) == "GY_Calc_tha|22661"] <-
+  "GY_Calc_tha"
 ## Create TD object
-wheatTD <- createTD(data = studyTableTot, genotype = "germplasmName", trial = "locationName",
-                    year = "year", repId = "replicate", subBlock = "blockNumber",
-                    design = "rcbd")
+wheatTD <- createTD(data = studyTableTot, genotype = "germplasmName",
+                    trial = "locationName", repId = "replicate",
+                    subBlock = "blockNumber", trDesign = "rcbd")
 ### Single Site Analysis
 ## Run Single Site Analysis using asreml
-SSA <- STRunModel(TD = wheatTD[wheatTD$trial == "Site01", ], traits = "GY_Calc_tha",
-                  engine = "asreml")
+SSA <- STRunModel(TD = wheatTD, traits = "GY_Calc_tha", engine = "asreml")
 ## Create a report for the single site analysis.
-report(SSA, outfile = "./testReports/SSASite01.pdf")
+report(SSA, outfile = "./testReports/SSASite01.pdf", trial = "Site01")
 ## Run Single Site Analysis for all trials and extract BLUPs for gxe analysis.
-BLUPList <- lapply(levels(wheatTD$trial), function(site) {
-  SSAEnv <- STRunModel(TD = wheatTD[wheatTD$trial == site, ], traits = "GY_Calc_tha",
+BLUPList <- lapply(names(wheatTD), function(site) {
+  SSAEnv <- STRunModel(wheatTD, trial = site, traits = "GY_Calc_tha",
                        what = "random", engine = "asreml")
-  STExtract(SSAEnv, what = "BLUPs", keep = "trial")
+  STExtract(SSAEnv, what = "BLUPs", keep = "trial")[[1]]
 })
 ## Create new TD object from BLUPs
-wheatTDBlup <- createTD(data = Reduce("rbind2", BLUPList))
+wheatTDBlup <- createTD(data = Reduce("rbind", BLUPList))
 
 ###GxE analysis
 ## Run AMMI analysis
@@ -60,6 +62,7 @@ summary(stab)
 report(stab, outfile = "./testReports/stabWheat.pdf")
 
 ## Run variance covariance analysis to find the best covariance structure.
-varComp <- gxeVarComp(TD = wheatTDBlup, trait = "GY_Calc_tha")
+varComp <- gxeVarComp(TD = wheatTDBlup, trait = "GY_Calc_tha",
+                      engine = "asreml")
 summary(varComp)
 report(varComp, outfile = "./testReports/varCompWheat.pdf")
