2b230461db569e0d0d4b37b2fac588201948a652
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2019-03-28 12:27:14 +0100

    Added tests for AMMI plot options

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -4,9 +4,10 @@ context("Plots")
 ## objects on which the plots are based are invisibly returned at least some
 ## checking can be done.
 
+geAmmi <- gxeAmmi(TD = TDMaize, trait = "yld")
+p0_1 <- plot(geAmmi, output = FALSE)
+p0_2 <- plot(geAmmi, plotType = "AMMI2", output = FALSE)
 test_that("AMMI plot gives correct output types", {
-  geAmmi <- gxeAmmi(TD = TDMaize, trait = "yld")
-  p1 <- plot(geAmmi, output = FALSE)
   expect_error(plot(geAmmi, plotType = "AMMI2", primAxis = "3"),
                "string starting with PC")
   expect_error(plot(geAmmi, plotType = "AMMI2", primAxis = "PC2"),
@@ -19,9 +20,92 @@ test_that("AMMI plot gives correct output types", {
                "primAxis should differ from secAxis")
   expect_error(plot(geAmmi, plotType = "AMMI2", secAxis = "PC3"),
                "run with 2 principal components")
-  p2 <- plot(geAmmi, plotType = "AMMI2", output = FALSE)
-  expect_is(p1, "ggplot")
-  expect_is(p2, "ggplot")
+  expect_is(p0_1, "ggplot")
+  expect_is(p0_2, "ggplot")
+})
+
+test_that("AMMI plot plotType options function properly", {
+  geGGE <- gxeAmmi(TD = TDMaize, trait = "yld", GGE = TRUE)
+  p1a <- plot(geAmmi, output = FALSE)
+  p1b <- plot(geAmmi, plotType = "GGE1", output = FALSE)
+  p2 <- plot(geGGE, output = FALSE)
+  expect_equal(p1a, p1b)
+  expect_equal(p2$labels$title, "GGE biplot for yld ")
+})
+
+test_that("AMMI plot plotGeno functions properly", {
+  p1_1 <- plot(geAmmi, plotGeno = FALSE, output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotGeno = FALSE, output = FALSE)
+  ## Difference with default plot p0 should be the missing GeomPoint layer.
+  geoms0_1 <- sapply(p0_1$layers, function(x) class(x$geom)[1])
+  geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
+  geoms0_2 <- sapply(p0_2$layers, function(x) class(x$geom)[1])
+  geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
+  expect_equal(setdiff(geoms0_1, geoms1_1), "GeomPoint")
+  expect_equal(setdiff(geoms0_2, geoms1_2), "GeomPoint")
+})
+
+test_that("AMMI plot sizeGeno functions properly", {
+  p1_1 <- plot(geAmmi, sizeGeno = 5, output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", sizeGeno = 5, output = FALSE)
+  ## Difference with default plot p0 should be the replaced GeomPoint layer
+  ## by GeomText layer.
+  geoms0_1 <- sapply(p0_1$layers, function(x) class(x$geom)[1])
+  geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
+  geoms0_2 <- sapply(p0_2$layers, function(x) class(x$geom)[1])
+  geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
+  expect_equal(geoms1_1[geoms0_1 == "GeomPoint"], "GeomText")
+  expect_equal(p1_1$layers[geoms0_1 == "GeomPoint"][[1]]$aes_params$size, 5)
+  expect_equal(geoms1_2[geoms0_2 == "GeomPoint"], "GeomText")
+  expect_equal(p1_2$layers[geoms0_2 == "GeomPoint"][[1]]$aes_params$size, 5)
+})
+
+test_that("AMMI plot plotEnv functions properly", {
+  p1_1 <- plot(geAmmi, plotEnv = FALSE, output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotEnv = FALSE, output = FALSE)
+  ## Difference with default plot p0 should be the missing GeomText layer.
+  ## For AMMI2 also the arrows, GeomSegment, should be missing.
+  geoms0_1 <- sapply(p0_1$layers, function(x) class(x$geom)[1])
+  geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
+  geoms0_2 <- sapply(p0_2$layers, function(x) class(x$geom)[1])
+  geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
+  expect_equal(setdiff(geoms0_1, geoms1_1), "GeomText")
+  expect_equal(setdiff(geoms0_2, geoms1_2), c("GeomText", "GeomSegment"))
+})
+
+test_that("AMMI plot sizeEnv functions properly", {
+  p1_1 <- plot(geAmmi, sizeEnv = 5, output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", sizeEnv = 5, output = FALSE)
+  geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
+  geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
+  expect_equal(p1_1$layers[geoms1_1 == "GeomText"][[1]]$aes_params$size, 5)
+  expect_equal(p1_2$layers[geoms1_2 == "GeomText"][[1]]$aes_params$size, 5)
+})
+
+test_that("AMMI plot envFactor functions properly", {
+  p1_1 <- plot(geAmmi, envFactor = 5, output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", envFactor = 5, output = FALSE)
+  ## Coordinate limits should blow up by a factor 5.
+  ## Lower x-limit is strongly dependent on genoscores so not blown up as much.
+  expect_equal(5 * p0_1$plot_env$p$coordinates$limits$x[2],
+               p1_1$plot_env$p$coordinates$limits$x[2])
+  expect_equal(5 * p0_1$plot_env$p$coordinates$limits$y,
+               p1_1$plot_env$p$coordinates$limits$y)
+  ## Coordinate limits should blow up by a factor 5.
+  expect_equal(5 * p0_2$plot_env$p$coordinates$limits$x,
+               p1_2$plot_env$p$coordinates$limits$x)
+  expect_equal(5 * p0_2$plot_env$p$coordinates$limits$y,
+               p1_2$plot_env$p$coordinates$limits$y)
+})
+
+test_that("AMMI plot colEnv functions properly", {
+  p1_1 <- plot(geAmmi, colEnv = "green", output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", colEnv = "green", output = FALSE)
+  geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
+  geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
+  expect_equal(p1_1$layers[geoms1_1 == "GeomText"][[1]]$aes_params$colour, "green")
+  expect_equal(p1_2$layers[geoms1_2 == "GeomText"][[1]]$aes_params$colour, "green")
+  expect_equal(p1_2$layers[geoms1_2 == "GeomSegment"][[1]]$aes_params$colour, "green")
 })
 
 test_that("FW plot gives correct output types", {
