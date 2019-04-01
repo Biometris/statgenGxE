ef142596caabcc84a03db2a68c05e073a02280c8
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2019-04-01 10:45:26 +0200

    Added test for outliers from random model (#63)

--- tests/testthat/test-outliers.R
+++ tests/testthat/test-outliers.R
@@ -28,6 +28,17 @@ test_that("outliersSSA functions properly for multiple traits", {
   expect_null(out14$outliers)
 })
 
+test_that("option what functions properly", {
+  out1 <- outlierSSA(modelLm, trial = "E1", traits = "t1", what = "random",
+                     verbose = FALSE)
+  expect_is(out1, "list")
+  expect_length(out1, 2)
+  expect_is(out1$indicator, "data.frame")
+  expect_equal(sum(out1$indicator[["t1"]]), 1)
+  expect_equal(nrow(out1$outliers), 1)
+  expect_equal(out1$outliers$res, 2.00884115446144)
+})
+
 test_that("option rLimit funtions properly", {
   out1 <- outlierSSA(modelLm, trial = "E1", traits = "t1", rLimit = 1,
                      verbose = FALSE)
@@ -52,4 +63,14 @@ test_that("option commonFactors functions properly", {
   expect_equal(sum(out1$outliers$similar), 8)
 })
 
+test_that("option verbose functions properly", {
+  printOut1 <- capture.output(out1 <- outlierSSA(modelLm, trial = "E1",
+                                                 traits = "t1", verbose = TRUE))
+  printOut2 <- capture.output(out1 <- outlierSSA(modelLm, trial = "E1",
+                                                 traits = "t1", what = "random",
+                                                 verbose = TRUE))
+  expect_equal(printOut1, "No large standardized residuals.")
+  expect_true("Large standardized residuals" %in% printOut2)
+  expect_true(any(grepl(pattern = "2.008841", x = printOut2)))
+})
 
