2d7c586ebc9bb67ebc3f8a32d19ee77c4c244927
Bart-Jan van Rossum <bart-jan.vanrossum@wur.nl>
2019-04-15 12:46:24 +0200

    Added test for stability summary

--- tests/testthat/test-summaries.R
+++ tests/testthat/test-summaries.R
@@ -1,8 +1,13 @@
 context("Summaries")
 
+testTD <- createTD(data = testData, genotype = "seed", trial = "field",
+                   rowId = "Y", colId = "X", rowCoord = "Y", colCoord = "X")
+modelSp <- STRunModel(testTD, design = "rowcol", traits = c("t1", "t2"))
+BLUEs <- SSAtoTD(modelSp, what = "BLUEs")
+
 test_that("AMMI summary produces correct output", {
-  geAmmi <- gxeAmmi(TD = TDMaize, trait = "yld")
-  geGGE <- gxeAmmi(TD = TDMaize, trait = "yld", GGE = TRUE)
+  geAmmi <- gxeAmmi(TD = BLUEs, trait = "t1")
+  geGGE <- gxeAmmi(TD = BLUEs, trait = "t1", GGE = TRUE)
   sumAmmi <- capture.output(summary(geAmmi))
   sumGGE <- capture.output(summary(geGGE))
   sumAmmi2 <- capture.output(summary(geAmmi, printGenoScores = TRUE))
@@ -13,4 +18,14 @@ test_that("AMMI summary produces correct output", {
   expect_false(all(c("Anova ", "Genotypic scores ") %in% sumGGE))
   expect_true(all(c("Principal components ", "Anova ",
                     "Environment scores ", "Genotypic scores ") %in% sumAmmi2))
+})
+
+test_that("Stability summary produces correct output", {
+  geStab <- gxeStability(TD = BLUEs, trait = "t1")
+  sumStab <- capture.output(summary(geStab))
+  sumStab2 <- capture.output(summary(geStab, pctGeno = 20))
+  expect_true(all(c("Cultivar-superiority measure (Top 10 % genotypes)",
+                    "Static stability (Top 10 % genotypes)",
+                    "Wricke's ecovalence (Top 10 % genotypes)") %in% sumStab))
+  expect_equal(length(sumStab2), length(sumStab) + 3)
 })
