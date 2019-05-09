dbab7425791aa1b0f7853dfb44b68794c9ebbac4
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-05-09 15:22:08 +0200

    Update .gitlab-ci.R

--- .gitlab-ci.R
+++ .gitlab-ci.R
@@ -5,6 +5,8 @@ installIfNeeded <- function(pkg,
   if (length(pkgPath) == 0) {
     message("NOTE: pkg ", pkg, " missing, installing...")
     install.packages(pkg, repos = repos, quiet = quiet)
+  } else {
+    update.packages(pkg, repos = repos, ask = FALSE)
   }
 }
 
@@ -16,9 +18,7 @@ pkgsUpdate <- function(repos = "https://cran.rstudio.com",
   if (instPkgdown) {
     installIfNeeded(pkg = "pkgdown", repos = repos, quiet = quiet)
   }
-  remotes::install_gitlab(repo = "statistical-genetic-pipeline/statgenSSA",
-                          host = "git.wur.nl")
-  remotes::install_deps(repos = repos, quiet = quiet, dependencies = TRUE)
+  remotes::install_deps(repos = repos, dependencies = TRUE, quiet = quiet)
   cat("INSTALLED:\n")
   instld <- as.data.frame(installed.packages())
   rownames(instld) <- NULL
--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname = find.package("statgenGxE"),
-                    pkgname = "statgenGxE"){
-  # CRAN Note avoidance
-  if (getRversion() >= "2.15.1")
-    utils::globalVariables(c(".", "..count.."))
-  invisible()
-}
