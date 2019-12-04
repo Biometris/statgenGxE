f5690d1fdefd118abe80e331d48d547067038376
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-12-04 13:59:03 +0100

    Added column year to dropsPheno

--- data-raw/dropsData.R
+++ data-raw/dropsData.R
@@ -3,10 +3,11 @@ phenoFile <- system.file("extdata", "grainYield_components_BLUEs.zip",
                          package = "statgenGWAS")
 dropsPheno <- read.csv(unz(description = phenoFile,
                            filename = "2b-GrainYield_components_BLUEs_level.csv"))
+## Add year column to demonstrate plot options.
+dropsPheno[["year"]] <- paste0("20", substring(dropsPheno[["Experiment"]],
+                                               first = 4, last = 5))
 ## Remove observations from 2011 from dropsPheno.
-dropsPheno <- dropsPheno[substring(dropsPheno[["Experiment"]],
-                                   first = 4, last = 5) != "11" &
-                           dropsPheno[["Experiment"]] != "Gra13W", ]
+dropsPheno <- dropsPheno[dropsPheno[["year"]] != "2011", ]
 dropsPheno <- droplevels(dropsPheno)
 
 usethis::use_data(dropsPheno, overwrite = TRUE)
