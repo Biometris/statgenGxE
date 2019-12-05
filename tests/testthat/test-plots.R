b10ab16cf07730df360fec666e5d5b1a7a7c69ca
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2019-12-05 14:59:07 +0100

    Removed output = FALSE from plotting tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -5,8 +5,8 @@ context("Plots")
 ## checking can be done.
 
 geAmmi <- gxeAmmi(TD = TDMaize, trait = "yld")
-p0_1 <- plot(geAmmi, output = FALSE)
-p0_2 <- plot(geAmmi, plotType = "AMMI2", output = FALSE)
+p0_1 <- plot(geAmmi)
+p0_2 <- plot(geAmmi, plotType = "AMMI2")
 test_that("AMMI plot gives correct output types", {
   expect_error(plot(geAmmi, plotType = "AMMI2", primAxis = "3"),
                "string starting with PC")
@@ -26,26 +26,26 @@ test_that("AMMI plot gives correct output types", {
 
 test_that("AMMI plot plotType options function properly", {
   geGGE <- gxeAmmi(TD = TDMaize, trait = "yld", GGE = TRUE)
-  p1a <- plot(geAmmi, output = FALSE)
-  p1b <- plot(geAmmi, plotType = "GGE1", output = FALSE)
-  p2 <- plot(geGGE, output = FALSE)
+  p1a <- plot(geAmmi)
+  p1b <- plot(geAmmi, plotType = "GGE1")
+  p2 <- plot(geGGE)
   expect_equal(p1a, p1b)
   expect_equal(p2$labels$title, "GGE biplot for yld ")
 })
 
 test_that("AMMI plot scale option functions properly", {
   ## Only relevant for AMMI2 plots.
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", scale = 0, output = FALSE)
-  p2_2 <- plot(geAmmi, plotType = "AMMI2", scale = 1, output = FALSE)
-  p3_2 <- plot(geAmmi, plotType = "AMMI2", scale = 0.75, output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", scale = 0)
+  p2_2 <- plot(geAmmi, plotType = "AMMI2", scale = 1)
+  p3_2 <- plot(geAmmi, plotType = "AMMI2", scale = 0.75)
   expect_equal(p1_2$labels$title, "AMMI2 biplot for yld (genotype scaling) ")
   expect_equal(p2_2$labels$title, "AMMI2 biplot for yld (environment scaling) ")
   expect_equal(p3_2$labels$title, "AMMI2 biplot for yld (51%) ")
 })
 
 test_that("AMMI plot plotGeno functions properly", {
-  p1_1 <- plot(geAmmi, plotGeno = FALSE, output = FALSE)
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotGeno = FALSE, output = FALSE)
+  p1_1 <- plot(geAmmi, plotGeno = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotGeno = FALSE)
   ## Difference with default plot p0 should be the missing GeomPoint layer.
   geoms0_1 <- sapply(p0_1$layers, function(x) class(x$geom)[1])
   geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
@@ -56,8 +56,8 @@ test_that("AMMI plot plotGeno functions properly", {
 })
 
 test_that("AMMI plot sizeGeno functions properly", {
-  p1_1 <- plot(geAmmi, sizeGeno = 5, output = FALSE)
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", sizeGeno = 5, output = FALSE)
+  p1_1 <- plot(geAmmi, sizeGeno = 5)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", sizeGeno = 5)
   ## Difference with default plot p0 should be the replaced GeomPoint layer
   ## by GeomText layer.
   geoms0_1 <- sapply(p0_1$layers, function(x) class(x$geom)[1])
@@ -73,8 +73,8 @@ test_that("AMMI plot sizeGeno functions properly", {
 })
 
 test_that("AMMI plot plotEnv functions properly", {
-  p1_1 <- plot(geAmmi, plotEnv = FALSE, output = FALSE)
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotEnv = FALSE, output = FALSE)
+  p1_1 <- plot(geAmmi, plotEnv = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotEnv = FALSE)
   ## Difference with default plot p0 should be the missing GeomText layer.
   ## For AMMI2 also the arrows, GeomSegment, should be missing.
   geoms0_1 <- sapply(p0_1$layers, function(x) class(x$geom)[1])
@@ -86,8 +86,8 @@ test_that("AMMI plot plotEnv functions properly", {
 })
 
 test_that("AMMI plot sizeEnv functions properly", {
-  p1_1 <- plot(geAmmi, sizeEnv = 5, output = FALSE)
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", sizeEnv = 5, output = FALSE)
+  p1_1 <- plot(geAmmi, sizeEnv = 5)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", sizeEnv = 5)
   geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
   geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
   dat1_1 <- p1_1$layers[geoms1_1 == "GeomText"][[1]]$data
@@ -97,8 +97,8 @@ test_that("AMMI plot sizeEnv functions properly", {
 })
 
 test_that("AMMI plot envFactor functions properly", {
-  p1_1 <- plot(geAmmi, envFactor = 5, output = FALSE)
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", envFactor = 5, output = FALSE)
+  p1_1 <- plot(geAmmi, envFactor = 5)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", envFactor = 5)
   ## Coordinate limits should blow up by a factor 5.
   ## Lower x-limit is strongly dependent on genoscores so not blown up as much.
   expect_equal(5 * p0_1$plot_env$p$coordinates$limits$x[2],
@@ -113,8 +113,8 @@ test_that("AMMI plot envFactor functions properly", {
 })
 
 test_that("AMMI plot colEnv functions properly", {
-  p1_1 <- plot(geAmmi, colEnv = "green", output = FALSE)
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", colEnv = "green", output = FALSE)
+  p1_1 <- plot(geAmmi, colEnv = "green")
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", colEnv = "green")
   geoms1_1 <- sapply(p1_1$layers, function(x) class(x$geom)[1])
   geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
   dat1_1 <- p1_1$layers[geoms1_1 == "GeomText"][[1]]$data
@@ -127,9 +127,9 @@ test_that("AMMI plot colEnv functions properly", {
 
 test_that("AMMI plot plotConvHull functions properly", {
   ## plotConvHull should be ignored for AMMI1.
-  expect_equal(p0_1, plot(geAmmi, plotConvHull = TRUE, output = FALSE))
+  expect_equal(p0_1, plot(geAmmi, plotConvHull = TRUE))
   ## For AMMI2 there should be two extra layers.
-  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotConvHull = TRUE, output = FALSE)
+  p1_2 <- plot(geAmmi, plotType = "AMMI2", plotConvHull = TRUE)
   geoms0_2 <- sapply(p0_2$layers, function(x) class(x$geom)[1])
   geoms1_2 <- sapply(p1_2$layers, function(x) class(x$geom)[1])
   expect_setequal(geoms1_2[-match(geoms0_2, geoms1_2)],
@@ -144,13 +144,13 @@ modelSp <- fitTD(testTDYear, design = "rowcol", traits = c("t1", "t2"))
 BLUEsYear <- SSAtoTD(modelSp, what = "BLUEs", keep = "year")
 geAmmiYear <- gxeAmmi(BLUEsYear, trait = "t1", byYear = TRUE)
 test_that("AMMI plot gives correct output types when byYear = TRUE", {
-  p1 <- plot(geAmmiYear, output = FALSE)
+  p1 <- plot(geAmmiYear)
   expect_is(p1, "list")
   expect_length(p1, 2)
   expect_named(p1, c("1", "2"))
   expect_is(p1[[1]], "ggplot")
   expect_is(p1[[2]], "ggplot")
-  p2 <- plot(geAmmiYear, plotType = "AMMI2", output = FALSE)
+  p2 <- plot(geAmmiYear, plotType = "AMMI2")
   expect_is(p2, "list")
   expect_length(p2, 2)
   expect_named(p2, c("1", "2"))
@@ -160,9 +160,9 @@ test_that("AMMI plot gives correct output types when byYear = TRUE", {
 
 test_that("FW plot gives correct output types", {
   geFw <- geFW <- gxeFw(TD = TDMaize, trait = "yld")
-  p1 <- plot(geFw, output = FALSE)
-  p2 <- plot(geFw, plotType = "line", output = FALSE)
-  p3 <- plot(geFw, plotType = "trellis", output = FALSE)
+  p1 <- plot(geFw)
+  p2 <- plot(geFw, plotType = "line")
+  p3 <- plot(geFw, plotType = "trellis")
   expect_is(p1, "list")
   expect_length(p1, 3)
   lapply(X = p1, FUN = expect_is, "ggplot")
@@ -172,7 +172,7 @@ test_that("FW plot gives correct output types", {
 
 SSA <- fitTD(TD = TDHeat05, design = "res.rowcol", traits = "yield")
 test_that("SSA base plot gives correct output types", {
-  p1 <- plot(SSA, traits = "yield", output = FALSE)
+  p1 <- plot(SSA, traits = "yield")
   expect_is(p1, "list")
   expect_length(p1, 1)
   expect_is(p1[[1]], "list")
@@ -183,7 +183,7 @@ test_that("SSA base plot gives correct output types", {
 })
 
 test_that("SSA spatial plot gives correct output types", {
-  p1 <- plot(SSA, plotType = "spatial", traits = "yield", output = FALSE)
+  p1 <- plot(SSA, plotType = "spatial", traits = "yield")
   expect_is(p1, "list")
   expect_length(p1, 1)
   expect_is(p1[[1]], "list")
@@ -194,8 +194,8 @@ test_that("SSA spatial plot gives correct output types", {
 })
 
 test_that("option what in SSA plot functions properly", {
-  p1 <- plot(SSA, what = "random", output = FALSE)
-  p2 <- plot(SSA, plotType = "spatial", what = "random", output = FALSE)
+  p1 <- plot(SSA, what = "random")
+  p2 <- plot(SSA, plotType = "spatial", what = "random")
   expect_is(p1, "list")
   expect_equal(p2[[1]][[1]][[5]]$labels$title, "Genotypic BLUPs")
   expect_equal(p2[[1]][[1]][[6]]$labels$x, "Genotypic BLUPs")
@@ -203,18 +203,18 @@ test_that("option what in SSA plot functions properly", {
 
 test_that("stability plot gives correct output types", {
   geStab <- gxeStability(TD = TDMaize, trait = "yld")
-  p1 <- plot(geStab, output = FALSE)
+  p1 <- plot(geStab)
   expect_is(p1, "list")
   expect_length(p1, 4)
   lapply(X = p1, FUN = expect_is, "ggplot")
   geStab2 <- gxeStability(TD = TDMaize, trait = "yld", method = "superiority")
-  p2 <- plot(geStab2, output = FALSE)
+  p2 <- plot(geStab2)
   expect_length(p2, 1)
 })
 
 test_that("varComp plot gives correct output types", {
   geVarComp <- gxeVarComp(TD = TDMaize, trait = "yld")
-  p <- plot(geVarComp, output = FALSE)
+  p <- plot(geVarComp)
   geoms <- sapply(p$layers, function(x) class(x$geom)[1])
   expect_is(p, "ggplot")
   expect_setequal(geoms, c("GeomTile", "GeomText"))
@@ -228,5 +228,5 @@ test_that("varComp plot gives correct output types when trials are numerical", {
     levels(TDMaize2[[trial]][["trial"]]) <- 1:8
   }
   geVarComp <- gxeVarComp(TD = TDMaize2, trait = "yld")
-  expect_silent(p <- plot(geVarComp, output = FALSE))
+  expect_silent(p <- plot(geVarComp))
 })
