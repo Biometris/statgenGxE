9f5f5756ba5f1a4628143baece2fccc31e0a5b31
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-01-23 10:44:58 +0100

    Fixed problem with missing decimals in FW report

--- R/reexport.R
+++ R/reexport.R
@@ -1,3 +1,7 @@
+#' @importFrom statgenSTA report
+#' @export
+statgenSTA::report
+
 #' @importFrom utils getFromNamespace
 #' @keywords internal
 createSTA <- getFromNamespace(x = "createSTA", ns = "statgenSTA")
--- R/statgenGxE.R
+++ R/statgenGxE.R
@@ -5,5 +5,5 @@
 #' @importFrom graphics plot
 #' @importFrom xtable xtable
 #' @importFrom utils hasName
-#' @importFrom statgenSTA createTD report fitTD STAtoTD
+#' @importFrom statgenSTA createTD fitTD STAtoTD
 NULL
