3aa0f5abd529ed42166c064432497635e74d2b4a
Rossum, Bart-Jan van <bart-jan.vanrossum@wur.nl>
2020-05-29 11:08:17 +0200

    Added extra tests for reporting

--- tests/testthat/test-report.R
+++ tests/testthat/test-report.R
@@ -13,9 +13,30 @@ test_that("Test that createReport function only accepts correctly named output",
   expect_silent(report(geAmmi, outfile = tempfile(fileext = ".pdf")))
 })
 
+## Finlay Wilkinson
+
+test_that("FW report functions correctly", {
+  ## Reporting requires pdflatex which isn't available on cran.
+  skip_on_cran()
+  geFw <- gxeFw(TD = BLUEs, trait = "t1")
+  expect_silent(report(geFw))
+})
+
 ## Stability
 
 test_that("Stability report functions correctly", {
+  ## Reporting requires pdflatex which isn't available on cran.
+  skip_on_cran()
   geStab <- gxeStability(TD = BLUEs, trait = "t1")
   expect_silent(report(geStab))
 })
+
+## varCov
+
+test_that("varCov report functions correctly", {
+  ## Reporting requires pdflatex which isn't available on cran.
+  skip_on_cran()
+  geVarCov <- gxeVarCov(TD = BLUEs, trait = "t1")
+  expect_silent(report(geVarCov))
+})
+
